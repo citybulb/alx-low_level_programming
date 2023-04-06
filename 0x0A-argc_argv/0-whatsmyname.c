ed22e77147edf24e94a5500821fb684f7ee4315a
Francis Kalu <talknaij@gmail.com>
2023-04-06 13:04:56 -0700

    My new name\

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function with argc and argv
+ * @argc: argument c in integer
+ * @argv: pointer array of argument v
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+
+{
+
+if (argc > 0)
+printf("%s\n", argv[0]);
+
+return (0);
+
+}
